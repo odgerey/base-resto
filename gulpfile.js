9a39705bfd09d49612704baf446a4d13d106e564
Audrey <audreymedaino@gmail.com>
2021-01-22 00:03:21 -0500

    Created a gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+
+// including plugins
+var gulp = require('gulp')
+var minifyCSS = require('gulp-minify-css')
+var autoprefixer = require('gulp-autoprefixer')
+var gp_concat = require('gulp-concat')
+var gp_rename = require('gulp-rename')
+var gp_uglify = require('gulp-uglify')
+var path = require('path')
